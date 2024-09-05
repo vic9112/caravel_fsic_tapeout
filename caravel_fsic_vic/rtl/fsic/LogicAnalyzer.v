fef8a62bdb3fe517985f4e4520475d80999330c5
nthuyouwei <145022311+nthuyouwei@users.noreply.github.com>
2024-09-05 17:51:40 +0800

    Update LogicAnalyzer.v

--- caravel_fsic_vic/rtl/fsic/LogicAnalyzer.v
+++ caravel_fsic_vic/rtl/fsic/LogicAnalyzer.v
@@ -19,12 +19,12 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-/* Function Description:
-  - Monitor signals provided by user project. Support up to 24 monitoring signals
-  - Support signal conditioning to trigger signal logging  (Currently, done by host program)
-  - Compress (Waveform compression, e.g. Run-Length-Encoding RLE) the logged signals and sent them to remote users using the Axis port. Waveform can be displayed in remote environments.
-  - Waveform log is designed to be saved in .vcd file format, it can be open by gtkwave.
-*/
+// Function Description:
+//   - Monitor signals provided by user project. Support up to 24 monitoring signals
+//   - Support signal conditioning to trigger signal logging  (Currently, done by host program)
+//   - Compress (Waveform compression, e.g. Run-Length-Encoding RLE) the logged signals and sent them to remote users using the Axis port. Waveform can be displayed in remote environments.
+//   - Waveform log is designed to be saved in .vcd file format, it can be open by gtkwave.
+
 
 module LOGIC_ANLZ #( parameter pADDR_WIDTH   = 15,
                      parameter pDATA_WIDTH   = 32
@@ -46,7 +46,7 @@ module LOGIC_ANLZ #( parameter pADDR_WIDTH   = 15,
     input  wire   [14: 0] axi_araddr,
     input  wire           axi_rready,
     input  wire           cc_la_enable,
-    input  wire           enable_la,
+    // input  wire           enable_la,     // Jiin : it is internal configuration register
 
     //AxiS  
     output wire   [31: 0] m_tdata,
@@ -56,7 +56,6 @@ module LOGIC_ANLZ #( parameter pADDR_WIDTH   = 15,
     output wire           m_tvalid,
     output wire   [1: 0]  m_tuser,
     output wire           la_hpri_req,
-
     input  wire           m_tready,
 
     //User singals
@@ -70,7 +69,7 @@ module LOGIC_ANLZ #( parameter pADDR_WIDTH   = 15,
     input  wire           uck2_rst_n,
     input  wire           axis_rst_n
 );
-    localparam FIFO_DEPTH               = 128;  
+    localparam FIFO_DEPTH               = 64;  
     localparam AXIS_PKT_LEN             = 8; 
     localparam H_THRESH_DEFAULT         = 7'h40;
     localparam L_THRESH_DEFAULT         = 7'b10; 	
@@ -80,131 +79,177 @@ reg [23:0] la_enable;
 reg [6:0] h_thresh;
 reg [6:0] l_thresh;
 reg [6:0] pop_cond;
+reg enable_la;              // Jiin configuratin register at 'h10  - it is used to reset enable_la
 reg [7:0] rc_count;
 reg [23:0] r_la_data;       // latched up_la_data;
-reg la_reset_n_d1;
-wire la_change;             // la signal changed
-wire la_reset_n;            // 
+wire la_change;             // la signal changed       
 wire trace_push;            // trace is push into FIFO
 wire fifo_full;
 wire [31:0] trace_packet;
-reg [7:0] fifo_count;
-reg [7:0] tx_count;     // count # of transfer
+reg [7:0] fifo_count;       // Jiin: parameterize the size of fifo_count with FIFO_depth use $clog2
+reg [7:0] tx_count;         // count # of transfer
 reg la_hpri_req_o;
-wire fifo_pop = m_tready & m_tvalid;// & (fifo_count > 0);
-wire fifo_push = trace_push & trace_push_ok;
 
-assign axi_awready  = axi_awvalid & axi_wvalid & la_reset_n;
-assign axi_wready   = axi_awvalid & axi_wvalid & la_reset_n;
 
-// axilite read - axi_rdata can be available when axi_arvalid (axi_araddr) is valid,
-//              - it is simply a combinational logic
+assign axi_awready  = axi_awvalid & axi_wvalid & enable_la;
+assign axi_wready   = axi_awvalid & axi_wvalid & enable_la;
+
+// axilite read - axi_rdata can be available when axi_arvalid (axi_araddr) is valid
+
 assign axi_rvalid   = axi_arvalid;
-assign axi_rdata    = (axi_araddr[11:2] == 10'h000) ? {8'b0, la_enable} : 
-                      (axi_araddr[11:2] == 10'h001) ? {25'b0, h_thresh} :
-                      (axi_araddr[11:2] == 10'h002) ? {25'b0, l_thresh} :
-                      (axi_araddr[11:2] == 10'h003) ? {25'b0, pop_cond} : 32'hFFFFFFFF;
+assign axi_rdata    = (axi_araddr[11:2] == 10'h000) ? {8'b0, la_enable} :   
+                      (axi_araddr[11:2] == 10'h001) ? {25'b0, h_thresh} :       // @'h4
+                      (axi_araddr[11:2] == 10'h002) ? {25'b0, l_thresh} :       // @'h8
+                      (axi_araddr[11:2] == 10'h003) ? {25'b0, pop_cond} :       // @'hc
+                      (axi_araddr[11:2] == 10'h004) ? {31'b0, enable_la} : 32'hFFFFFFFF;  // Jiin @'h10
 assign axi_arready  = axi_arvalid;
 
-always @ ( posedge axi_clk ) 
-    la_reset_n_d1 <= la_reset_n;
-
-always @ ( posedge axi_clk or negedge enable_la ) 
-    if( !enable_la) 
-        begin
+// Jiin : enable_la is the internal configuration register, we can not use enable_la to reset configuration register
+//   use axi_reset_n instead
+always @ ( posedge axi_clk or negedge axi_reset_n ) begin
+    if( !axi_reset_n) begin
         la_enable   <= 24'h0;
         h_thresh    <= H_THRESH_DEFAULT;
         l_thresh    <= L_THRESH_DEFAULT;
         pop_cond    <= AXIS_PKT_LEN;
-        end 
-    else if( cc_la_enable & axi_awvalid & axi_wvalid) 
-        begin
+        enable_la   <= 1'b0;
+    end 
+    else if( cc_la_enable & axi_awvalid & axi_wvalid) begin
         if( axi_awaddr[11:2] == 10'h000) la_enable <= axi_wdata[23:0];
         if( axi_awaddr[11:2] == 10'h001) h_thresh  <= axi_wdata[6:0];
         if( axi_awaddr[11:2] == 10'h002) l_thresh  <= axi_wdata[6:0];
         if( axi_awaddr[11:2] == 10'h003) pop_cond  <= axi_wdata[6:0];
-        end
+        if( axi_awaddr[11:2] == 10'h004) enable_la <= axi_wdata[0];         // Jiin: add configuration : enable_la
+    end
+end
 
-
-
-assign la_change = (fifo_full) ? la_reset_n : ( up_la_data != r_la_data );
-assign la_reset_n = enable_la & ~la_hpri_req_o;
+assign la_change = |(la_enable & ( up_la_data != r_la_data)); 
 
 // fifo full handling
 //  - push null-packet all 0
 //  - flush FIFO 
 //  - restart - 
-
-// generate full_reset -> fifo_full till fifo_empty (flush fifo)
-// la_reset_n= cc_la_enable & ~full_reset
-// Note: we use cc_la_enable to reset, so that we can use software to reset LA
-// rc_count - repeat count
-// increament when there is no la_change
-always @ ( posedge axi_clk or negedge la_reset_n)    // note: cc_la_enable to reset
-    if( !la_reset_n ) 
-        begin
-        rc_count <= 8'h01;
+// issue: when fifo_full, do we still count rc?
+// - rc continues count
+// - signal waveform recovery
+//   - when receive null-packet -> generate a cycle of 'x' signals
+//   - generate signal waveforms with non-null packet with rc 
+always @ ( posedge axi_clk or negedge enable_la) begin  // note: cc_la_enable to reset
+    if( !enable_la ) begin
+        rc_count  <= 8'h01;
         r_la_data <= 24'b0;
-        end
-    else  
-        if( !la_change & rc_count != 8'hff)       // signal is not changed
+    end else begin
+        if( !la_change & rc_count != 8'hff)             // signal is not changed
             rc_count <= rc_count + 1;
-        else 
-            begin
+        else begin
             r_la_data <= up_la_data;
             rc_count <= 8'h01;
-            end
-        
-     
+        end
+    end 
+end
+
 
 // trace_push : condition to push a trace into fifo
 // 1. signal changes - la_change
 // 2. rc_count reach 255
-
-
-
+// 3. fifo_full -> push null packet (issue : use fifo_full - 1 or fifo_count == l_thread)
 
 // --- FIFO Instance -----
-la_fifo
-#( .WIDTH(32)
-   //.DEPTH(128)              // Jiin - use new fifo ip with parameter DEPTH
-)   fifo_inst
+
+
+`ifdef USE_PDK_SRAM
+    localparam sram_datawidth=64;
+`else
+    localparam sram_datawidth=128;
+`endif
+    wire sram_we;
+    wire [$clog2(FIFO_DEPTH)-1:0] sram_addr;
+    wire [sram_datawidth-1:0]sram_din;
+    wire [sram_datawidth-1:0] sram_dout;
+
+// --- FIFO Instance -----
+fifo #(.WIDTH(32),.depth(FIFO_DEPTH),.sram_datawidth(sram_datawidth),.mode(0)) la_fifo
 (
     .axis_clk (axis_clk) ,
-    .axi_reset_n (axi_reset_n),
+    .axi_reset_n (enable_la),
     .w_vld (trace_push),          // fifo push
     .w_rdy (trace_push_ok),          // w_rdy
     .data_in (trace_packet),
     .r_rdy (m_tready),          // directly connect to axis r_ready
     .r_vld (m_tvalid),          // connect to axis r_valid
-    .data_out (m_tdata)
+    .data_out (m_tdata),
+    .TH_reg(8'd0),
+    .sram_we(sram_we),
+    .sram_addr(sram_addr),
+    .sram_din(sram_din),
+    .sram_dout(sram_dout)
 );
 
-// FIFO depth counter - it is used to asserts la_hpri_req
+`ifdef USE_PDK_SRAM
 
+    ra1shd32x64m4h3v2 la_SRAM32x64(
+        .CLK(axis_clk),
+        .WEN(~sram_we),// WEB =1 is read WEB=0 is w_vldite
+        .OEN(1'b0),
+        .CEN(1'b0),
+        .A(sram_addr),
+        .I(sram_din),
+        .Q(sram_dout)
+    );
 
-assign trace_push = (la_change | (rc_count == 8'hff)) & la_reset_n;
-assign fifo_full = fifo_count >= h_thresh;        
-assign trace_packet = {32{!fifo_full}} & {rc_count, (r_la_data & la_enable)}; 
+`else
+    SRAM1RW64x128 la_SRAM1RW64x128(
+        .CE(axis_clk),
+        .WEB(~sram_we),// WEB =1 is read WEB=0 is w_vldite
+        .OEB(1'b0),
+        .CSB(1'b0),
+        .A(sram_addr),
+        .I(sram_din),
+        .O(sram_dout)
+    );
+`endif
+// fifo pop & fifo push , m_tvalid is the fifo_empty , trace_push_ok is the fifo_full - JIANG
+wire fifo_pop = m_tready & m_tvalid;
+wire fifo_push = trace_push & trace_push_ok;
+
+//  FIFO depth counter - it is used to asserts la_hpri_req
+//  Jiin: explain the trace push logic
+//  push condition
+//  1. signal change: la_change
+//  2. rc (repeat-count) reach maximum, i.e. rc = 8'hff
+//  3. fifo_full_push - fifo_full & push (null_pakcet) - JIANG
+//      r_block_push - block fifo push when fifo full
+//      from fifo_full -> la_hpri_req_o (change to 1)
+//      at the end of the r_block_push, push a null-packet (use la_hpri_req_o and r_block_push) - JIANG
+//      from fifo_full -> la_hpri_req_o (change to 0)
+
+reg r_block_push; 
+wire fifo_full_push;
+
+always @ ( posedge axi_clk or negedge enable_la )begin
+    if(!enable_la) begin
+        r_block_push <= 0;
+    end else if( trace_push_ok == 1'b0) r_block_push <= 1;
+        else if( la_hpri_req_o == 1'b0) r_block_push <= 0; // delay one cycle(from fifo_count < l_thresh) - JIANG
+    end
+
+assign fifo_full_push = ((r_block_push == 1'b1) & (la_hpri_req_o == 1'b0)); // (fifo_full_push == 1'b0 & (la_hpri_req_o == 1'b1) the end of r_block_push
+assign trace_push = (la_change | (rc_count == 8'hff)) & (!r_block_push | fifo_full_push); 
+assign trace_packet = {32{!fifo_full_push}} & {rc_count, (r_la_data & la_enable)}; 
 
 assign la_hpri_req = la_hpri_req_o;
 always @ ( posedge axi_clk or negedge enable_la )
-    if(!enable_la) 
-        begin
+    if(!enable_la) begin
         fifo_count <= 8'b0;
         la_hpri_req_o <= 0; 
-        end
-    else 
-        begin
-        if(fifo_full) la_hpri_req_o <= 1 ;
-        if(fifo_count<l_thresh) la_hpri_req_o <= 0;
+    end
+    else begin
+        // if(fifo_full) la_hpri_req_o <= 1 ;
+        if(fifo_count >= h_thresh)  la_hpri_req_o <= 1;
+        if(fifo_count < l_thresh) la_hpri_req_o <= 0;
         if( fifo_push & !fifo_pop) fifo_count <= fifo_count + 1;
         if(!fifo_push &  fifo_pop) fifo_count <= fifo_count - 1;
-        end
-
-
-
-
+    end
 
 assign m_tuser = 2'b00;
 assign m_tstrb = 4'b1111;
@@ -218,7 +263,6 @@ assign m_tlast = (tx_count == pop_cond);
 // 3. fifo empty 
 // Check upstream Axi-switch will still transfer data when there is no tlast
 
-
 always @ ( posedge axi_clk or negedge enable_la )
     if( !enable_la ) 
         tx_count <= 1;
@@ -227,202 +271,6 @@ always @ ( posedge axi_clk or negedge enable_la )
             tx_count <= 1;
         else if(fifo_pop)
             tx_count <= tx_count + 1;
-            
+
 // ----    Jiin - comment out the following
 endmodule
-
-
-
-
-module la_fifo
-#( parameter WIDTH = 32
-)
-(
-    input axis_clk ,
-    input axi_reset_n ,
-    // write protocol
-    input   w_vld,
-    output  w_rdy,
-    input [WIDTH-1:0] data_in,
-    // read protocol
-    input r_rdy,
-    output  r_vld,
-    output [WIDTH-1:0] data_out
-    );   
-    localparam IDLE = 2'd0, WAIT_SRAM_READ = 2'd1, WAIT_FIFO_READ = 2'd2, NORMAL = 2'd3; // state machine part
-    localparam sram_addr_width=$clog2(16);
-    localparam sram_wordLength=64;
-    localparam ptr_width=sram_addr_width+2; // LSB note even/odd times, MSB to decide full or not 
-    reg [1:0] state;
-    reg [1:0] state_next;
-    reg [ptr_width-1:0] w_ptr;
-    reg [ptr_width-1:0] r_ptr;
-    wire [ptr_width-1:0] w_ptr_next;
-    wire [ptr_width-1:0] r_ptr_next;
-    assign w_ptr_next=w_ptr+1'b1;
-    assign r_ptr_next=r_ptr+1'b1;
-    wire sram_we;
-    
-
-    ///////////////////  state machine////////// 
-    //If it may directly reads from data_l, we need to ensure that it doesn't skip some data when it needs to read from SRAM.
-    //After reads from data_l, Since the normal condition should starts with r_ptr[0] == 0, we need to add this state machine to make it corretly exec in normal condition. 
-    // w_ptr==r_ptr_next ie reads from data_l.
-    always @(posedge axis_clk or negedge axi_reset_n) begin
-        if (!axi_reset_n)begin 
-            state<=2'd0;
-        end
-        else begin
-            state<=state_next;
-        end
-    end 
-    wire empty;
-    always @(*) begin
-        case(state)
-            IDLE: begin   
-                if(!(w_ptr==r_ptr_next|empty))begin
-                    if(r_ptr[0]) state_next=WAIT_SRAM_READ;
-                    else state_next=NORMAL;
-                end
-                else state_next=state;
-            end
-            WAIT_SRAM_READ: begin
-                if(~sram_we) state_next=WAIT_FIFO_READ;
-                else state_next=state;
-            end
-            WAIT_FIFO_READ:begin
-                if(r_rdy) state_next=NORMAL;
-                else state_next=state;
-            end
-            NORMAL:begin
-                if(w_ptr==r_ptr_next) state_next=IDLE;
-                else state_next=state;
-            end
-        endcase
-    end 
-
-    /////////////////////////// ptr part ///////////////////////////////////////////////////////////////////////////
-    always @(posedge axis_clk or negedge axi_reset_n) begin
-        if (!axi_reset_n)begin 
-            w_ptr<=0;
-        end
-        else begin
-            if(w_vld & w_rdy)w_ptr<=w_ptr_next;
-            else  w_ptr<=w_ptr;
-        end
-    end 
-    always @(posedge axis_clk or negedge axi_reset_n) begin
-        if (!axi_reset_n)begin 
-            r_ptr<=0;
-        end
-        else begin
-            if(r_rdy & r_vld)r_ptr<=r_ptr_next;
-            else  r_ptr<=r_ptr;
-        end
-    end 
-
-///////////////////////////////// Write part ////////////////////////////////////////////////////////////////
-//the MSB is different while the other bits are the same, it indicates that w_ptr has already completed one full cycle ahead of r_ptr, meaning it is full.
-    assign w_rdy=~((w_ptr[ptr_width-1]==~r_ptr[ptr_width-1])&(w_ptr[ptr_width-2:0]==r_ptr[ptr_width-2:0])); 
-    
-    reg [WIDTH-1:0]data_in_l;
-
-    always @(posedge axis_clk or negedge axi_reset_n) begin
-        if (!axi_reset_n)begin 
-            data_in_l<=0;
-        end
-        else begin
-            if(w_vld & w_rdy)
-            data_in_l<=data_in;
-            else 
-            data_in_l<=data_in_l;
-        end
-    end 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-    wire [sram_addr_width-1:0] sram_addr;
-    wire [sram_wordLength-1:0]sram_din;
-    assign sram_we= w_vld & w_ptr[0]; 
-    assign sram_addr=(sram_we)?w_ptr[ptr_width-2:1]:(state==NORMAL)?r_ptr_next[ptr_width-2:1]:r_ptr[ptr_width-2:1];
-    assign sram_din ={data_in,data_in_l};
-    
-    
-    
-    
-    /////////////////// Read part ///////////////////////////
-    wire [sram_wordLength-1:0] sram_dout;
-    wire [WIDTH-1:0] sram_out_even;
-    wire [WIDTH-1:0] sram_out_odd;
-    assign sram_out_even = sram_dout[WIDTH-1:0];
-    assign sram_out_odd = sram_dout[2*WIDTH-1:WIDTH];
-
-
-
-    reg [WIDTH-1:0] data_out_odd;
-
-    always @(posedge axis_clk or negedge axi_reset_n) begin
-        if (!axi_reset_n)begin 
-            data_out_odd<=0;
-        end
-        else begin
-            if(~r_ptr[0])
-            data_out_odd<=sram_out_odd;
-            else 
-            data_out_odd<=data_out_odd;
-        end
-    end 
-
-    reg r_vld_even;
-    assign empty=(w_ptr==r_ptr);
-    assign r_vld=((r_ptr_next==w_ptr)?1'b1:(state==NORMAL)?((r_ptr[0])?1'b1:r_vld_even):state==WAIT_FIFO_READ)& !empty;
-    assign data_out=(r_ptr_next==w_ptr)?data_in_l: (state==NORMAL)?((r_ptr[0])?data_out_odd:sram_out_even):sram_out_odd;  
-
-  
-
-    always@(posedge axis_clk or negedge axi_reset_n) begin
-        if (!axi_reset_n) begin 
-            r_vld_even <= 1'b0;
-        end
-        else begin
-            if(!empty) begin
-                if(state==NORMAL)begin
-                    if(~r_ptr[0] & r_vld &r_rdy)
-                        r_vld_even <= 1'b0;
-                    else if (~sram_we & !(r_ptr_next[ptr_width-2:1]==w_ptr[ptr_width-2:1]))  // To avoid reading before writing.
-                        r_vld_even <= 1'b1;
-                    else 
-                        r_vld_even<=r_vld_even;
-                end
-                else r_vld_even<=1'b0;
-            end
-            else begin
-                r_vld_even <= 1'b0;
-            end
-        end
-    end
-
-`ifdef USE_PDK_SRAM
-    ra1shd16x64m4h3v2 SRAM16x64(
-        .CLK(axis_clk),
-        .WEN(~sram_we),
-        .OEN(1'b0),
-        .CEN(1'b0),
-        .A(sram_addr),
-        .D(sram_din),
-        .Q(sram_dout)
-    );
-`else
-    SRAM1RW64x128 la_SRAM1RW64x128(
-        .CE(axis_clk),
-        .WEB(~sram_we),// WEB =1 is read WEB=0 is w_vldite
-        .OEB(1'b0),
-        .CSB(1'b0),
-        .A(sram_addr),
-        .I(sram_din),
-        .O(sram_dout)
-    );
-`endif
-
-endmodule
-
