60a679e629b10ecf7477690221f703e89311c458
Vic Chen <137171415+vic9112@users.noreply.github.com>
2024-09-09 02:34:44 +0800

    Update pads_config.v

--- caravel_fsic_vic/rtl/top/pads_config.v
+++ caravel_fsic_vic/rtl/top/pads_config.v
@@ -42,3 +42,40 @@ module pads_config (
   output [43:0] re,
   output [43:0] oe
 );
+
+  reg [43:0] r_REN;
+  reg [43:0] r_OEN;
+
+  assign oe = r_OEN;
+
+  // Reset period, force ren=0, i.e. enable pull-up/down resistors
+  generate
+    genvar i;
+    for (i = 0; i < 44; i = i + 1) begin : AND_RST
+      assign re[i] = r_REN[i] & resetb;
+    end
+  endgenerate
+
+  // Given the initial state when reset (I/O)
+  always @(posedge clk or negedge resetb) begin
+    if (~resetb) begin
+      r_OEN[0]     <=   {{1'b1}};  // JTAG
+      r_OEN[1]     <=   {{1'b0}};  // SDO
+      r_OEN[5:2]   <=  {4{1'b1}};  // SDI, CSB, SCK, ser_rx
+      r_OEN[6]     <=   {{1'b0}};  // ser_tx
+      r_OEN[21:7]  <= {15{1'b1}};  // irq, RXD, RXCLK
+      r_OEN[35:22] <= {14{1'b0}};  // TXD, TXCLK
+      r_OEN[36]    <=   {{1'b1}};  // IOCLK
+      r_OEN[37]    <=   {{1'b1}};  // mprj[37
+      r_OEN[38]    <=   {{1'b1}};  // clock
+      r_OEN[41:39] <=  {3{1'b0}};  // flash_csb, flash_clk, flash_io0
+      r_OEN[43:42] <=  {2{1'b1}};  // flash_io1, gpio
+    end else begin
+      integer I;
+      for (i = 0; i < 44; i = i + 1) begin
+        if (cnfg_en[i]) r_OEN[i] <= cnfg_io[i];
+      end
+    end
+  end
+
+endmodule
