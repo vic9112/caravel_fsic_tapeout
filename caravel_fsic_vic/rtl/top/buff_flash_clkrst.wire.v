e62651045767b64c81ecc5f2800c1acc7d8be499
Vic Chen <137171415+vic9112@users.noreply.github.com>
2024-09-24 03:16:47 +0800

    Update buff_flash_clkrst.wire.v

--- caravel_fsic_vic/rtl/top/buff_flash_clkrst.wire.v
+++ caravel_fsic_vic/rtl/top/buff_flash_clkrst.wire.v
@@ -1,17 +1,17 @@
 //===========================================================
 // Modified by Vic Chen
-// July 26, 2024
+// Sep 23, 2024
 //===========================================================
 
 `default_nettype wire
 
 module buff_flash_clkrst (
-	input  [11:0] in_n,
+	input  [5:0] in_n,
 	input  [2:0]  in_s,
-	output [11:0] out_s,
+	output [5:0] out_s,
 	output [2:0]  out_n);
 
-	clkbuffer BUF[14:0] (
+	clkbuffer BUF[8:0] (
 		.A({in_n, in_s}), 
 		.X({out_s, out_n})
 	);   
