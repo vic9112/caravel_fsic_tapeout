ae830936851b323b57935864d02e8569582aa749
nthuyouwei <145022311+nthuyouwei@users.noreply.github.com>
2024-09-24 20:54:03 +0800

    Update buff_flash_clkrst.wire.v

--- caravel_fsic_vic/rtl/top/buff_flash_clkrst.wire.v
+++ caravel_fsic_vic/rtl/top/buff_flash_clkrst.wire.v
@@ -10,13 +10,15 @@ module buff_flash_clkrst (
 	input  [2:0]  in_s,
 	output [5:0] out_s,
 	output [2:0]  out_n);
-
+`ifdef FPGA
+	assign out_s = in_n;
+        assign out_n = in_s;    
+`else
 	clkbuffer BUF[8:0] (
 		.A({in_n, in_s}), 
 		.X({out_s, out_n})
 	);   
-
-    //assign out_s = in_n;
-    //assign out_n = in_s;     
+`endif
+ 
 
 endmodule
