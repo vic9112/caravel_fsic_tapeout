aba681ae8acf47cfb42be9b3169174cfbccb023b
ubuntu <gungod0487@gmail.com>
2024-10-08 07:55:20 -0400

    delete no use file

--- impl_FPGA/vivado/vitis_prj/hls_userdma/hls_userdma.prj/solution1/sim/verilog/xsim.dir/userdma/xsim_script.tcl
+++
@@ -1,4 +0,0 @@
-set_param project.enableReportConfiguration 0
-load_feature core
-current_fileset
-xsim {userdma} -view {{userdma_dataflow_ana.wcfg}} -tclbatch {userdma.tcl} -protoinst {userdma.protoinst}
--- impl_FPGA/vivado/vvd_srcs/caravel_soc/rtl/user/user_subsys/user_prj/user_prj2/rtl/rtl.f
+++
@@ -1,4 +0,0 @@
-user_prj2.v
-stage.v
-In_copy.v
-spram.v
